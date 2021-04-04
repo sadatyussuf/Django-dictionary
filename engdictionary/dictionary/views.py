9c2cdd026ab0d3da379fbe78a5abf0542ea47148
sadatyussuf <tommyleespartan0@gmail.com>
2021-04-04 19:42:42 +0000

    worked on the views.py file

--- engdictionary/dictionary/views.py
+++ engdictionary/dictionary/views.py
@@ -1,3 +1,19 @@
+from re import search
 from django.shortcuts import render
-
+from PyDictionary import PyDictionary as pyd
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def word(request):
+    search = request.GET.get('search')
+    dictionary = pyd()
+    meaning = dictionary.meaning(search)
+    synonmys = dictionary.synonym(search)
+    antonyms = dictionary.antonym(search)
+    context = {
+        'meaning': meaning,
+        'synonmys' : synonmys,
+        'antonyms' : antonyms
+    }
+    return render(request,'word.html',context)
